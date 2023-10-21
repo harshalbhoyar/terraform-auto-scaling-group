30fdc3a820b6141fa53db09946a0f2c1d7e849ac
Harshal Bhoyar <harshalbhoyar14@gmail.com>
2023-10-21 19:29:54 +0530

    Added tags to auto-scaling group EC2 instance

--- main.tf
+++ main.tf
@@ -1,50 +1,4 @@
 
-
-
-# provider "aws" {
-#   # Specifies the desired AWS region for resource provisioning
-#   region = "us-east-1"
-# }
-
-
-# resource "aws_instance" "test-instance" {
-#   ami             = var.ami
-#   instance_type   = var.instance_type
-#   key_name        = var.key_pair
-#   security_groups = [aws_security_group.terraform-security-group.name]
-
-#   tags = {
-#     Name = "terraform-ec2"
-#   }
-# }
-
-
-# resource "aws_security_group" "terraform-security-group" {
-#   name = "terraform-security-group"
-
-#   #Incoming traffic
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"] #replace it with your ip address
-#   }
-
-#   #Outgoing traffic
-#   egress {
-#     from_port   = 0
-#     protocol    = "-1"
-#     to_port     = 0
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   tags = {
-#     Name = "terraform-security-group"
-#   }
-
-# }
-
-
 terraform {
   required_providers {
     aws = {
@@ -52,6 +6,7 @@ terraform {
     }
   }
 }
+
 
 
 resource "aws_security_group" "terraform-security-group" {
@@ -76,8 +31,8 @@ resource "aws_security_group" "terraform-security-group" {
   tags = {
     Name = "terraform-security-group"
   }
-
 }
+
 
 
 
@@ -89,7 +44,20 @@ resource "aws_launch_template" "terraform-launch-template" {
   #   security_groups = [aws_security_group.terraform-security-group.name]
   vpc_security_group_ids = [aws_security_group.terraform-security-group.id]
 
+
+  # Tag specifications for the instance
+  tag_specifications {
+    # Specifies the resource type as "instance"
+    resource_type = "instance"
+
+    # Tags to apply to the instance
+    tags = {
+      Name = "first template"
+    }
+  }
 }
+
+
 
 resource "aws_autoscaling_group" "autoscaling-group" {
   availability_zones = ["us-east-1a"]
@@ -101,10 +69,6 @@ resource "aws_autoscaling_group" "autoscaling-group" {
     id      = aws_launch_template.terraform-launch-template.id
     version = "$Latest"
   }
-
-  # tag = {
-  #   Name = "test-instance"
-  # }
 
 }
 
